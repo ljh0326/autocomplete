4a428088679d0eec9949a8ce80e134b057b49775
Lee <ljh0326s@gmail.com>
2019-09-19 23:50:51 +0900

    addEntity: add document

--- src/main/java/com/simple/autocomplete/Document/Entity/DocumentVo.java
+++ src/main/java/com/simple/autocomplete/Document/Entity/DocumentVo.java
@@ -1,12 +1,48 @@
 package com.simple.autocomplete.Document.Entity;
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import com.simple.autocomplete.model.BaseEntity;
+import org.springframework.data.jpa.repository.Temporal;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.persistence.TemporalType;
+import java.util.Date;
 
 @Entity
 @Table(name = "DOCUMENT_TABLE")
-public class DocumentVo {
+public class DocumentVo extends BaseEntity {
 
+    @Column(name = "TITLE", nullable = false)
+    private String title;
+
+    @Column(name = "REG_DATE", nullable = false)
+    private Date regDate;
+
+    @Column(name = "EDIT_DATE", nullable = false)
+    private Date editDate;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getRegDate() {
+        return regDate;
+    }
+
+    public void setRegDate(Date regDate) {
+        this.regDate = regDate;
+    }
+
+    public Date getEditDate() {
+        return editDate;
+    }
+
+    public void setEditDate(Date editDate) {
+        this.editDate = editDate;
+    }
 }
