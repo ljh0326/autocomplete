75bf875a144c6bf1157e14c68768ed61ab859f0b
Lee <ljh0326s@gmail.com>
2019-09-20 21:47:07 +0900

    updateController: add Controller

--- src/main/java/com/simple/autocomplete/Document/Controller/DocumentController.java
+++ src/main/java/com/simple/autocomplete/Document/Controller/DocumentController.java
@@ -1,4 +1,29 @@
 package com.simple.autocomplete.Document.Controller;
 
+import com.simple.autocomplete.Document.Service.DocumentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Set;
+
+@Controller
+@RequestMapping("/document")
 public class DocumentController {
+
+    private DocumentService documentService;
+
+    @Autowired
+    public DocumentController(DocumentService documentService) {
+        this.documentService = documentService;
+    }
+
+    @GetMapping("/autocomplete")
+    public ResponseEntity<Set<String>> autocomplete(@RequestParam("searchWord")String searchWord){
+        return new ResponseEntity<>(documentService.autoComplete(searchWord), HttpStatus.OK);
+    }
 }
