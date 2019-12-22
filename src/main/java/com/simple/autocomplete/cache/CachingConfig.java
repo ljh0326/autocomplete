3b60436557d10724a41a771f5080800b4d903c2b
Lee <ljh0326s@gmail.com>
2019-12-22 14:15:11 +0900

    [back-end] add cache config

---
+++ src/main/java/com/simple/autocomplete/cache/CachingConfig.java
@@ -0,0 +1,15 @@
+package com.simple.autocomplete.cache;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author AIS 정훈
+ * @contact jhlee@saltlux.com
+ * CacheConfig
+ * EHCACHE3는 다른 설정을 따로 써줄 필요가 없다.
+ */
+@Configuration
+@EnableCaching
+public class CachingConfig
+{
+}
